4fa689d74dc9ff35de76423292f02e139c7f3cdd
Gökhan Akçapınar <gokhanakcapinar@gmail.com>
2026-04-17 07:44:52 +0300

    While Döngüsü

--- Hafta10/Program.cs
+++ Hafta10/Program.cs
@@ -4,8 +4,93 @@
     {
         static void Main(string[] args)
         {
+            //if (true)
+            //{
+
+            //}
 
 
+            //int a = 1;
+
+            //while (true)
+            //{
+            //    Console.WriteLine("BREAK ÖNCESİ");
+
+            //    if (true)
+            //    {
+            //        continue;
+            //    }
+
+
+
+            //    Console.WriteLine("BREAK SONRASI");
+            //}
+
+
+            //int i = 0;
+
+            //while (i <= 100)
+            //{
+            //    if (i%2 == 1)
+            //    {
+            //        Console.WriteLine($"{i} = TEK");
+            //    }
+            //    else if (i % 2 == 0)
+            //    {
+            //        Console.WriteLine($"{i} = ÇİFT");
+            //    }
+
+            //    i++;
+            //}
+
+            //int sayi = new Random().Next(100);
+            //int tahmin = 0;
+
+            //int deneme = 1;
+
+            //while (true)
+            //{
+            //    if (deneme > 5)
+            //    {
+            //        Console.WriteLine("Deneme hakkınız bitti...");
+            //        break;
+            //    }
+
+
+            //    Console.Write($"{deneme}. Tahminini gir: ");
+            //    string tahminS = Console.ReadLine();
+            //    tahmin = Convert.ToInt32(tahminS);
+
+
+            //    if (tahmin == sayi)
+            //    {
+            //        Console.WriteLine("Tebrikler doğru cevap");
+            //        break;
+            //    }
+
+            //    Console.WriteLine();
+            //    deneme++;
+            //}
+
+
+            while (true)
+            {
+
+                if (true)
+                {
+                    break;
+                }
+
+            }
+
+
+
+            Console.ReadLine();
+
+            //switch (switch_on)
+            //{
+            //    default:
+            //}
         }
     }
 }
